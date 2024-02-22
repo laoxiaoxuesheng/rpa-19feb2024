effb314969d13a98840becb044a63bc6b3f2beaf
laoxiaoxuesheng <153107238+laoxiaoxuesheng@users.noreply.github.com>
2024-02-22 10:45:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import time
 from openai import OpenAI
 
-openai_api_key=os.environ["OPENAI_API_KEY"]
+openai_api_key=os.environ("OPENAI_API_KEY")
 os.environ["REPLICATE_API_TOKEN"]="r8_ZanqsRocpoyeTahJ7ya7sPAlCxGXC7508W2sa"
 model = OpenAI(api_key=openai_api_key)
 
